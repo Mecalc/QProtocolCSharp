d936b806e2cc9e9371305ac6acd8fe616ee70e8f
Ernst Swanepoel <eswanepoel@mecalc.de>
2023-07-25 16:35:45 +0200

    Added GPS Channel Support

---
+++ DataStreaming/DataPackets/GpsDataPacket.cs
@@ -0,0 +1,54 @@
+﻿// -------------------------------------------------------------------------
+// Copyright (c) Mecalc (Pty) Limited. All rights reserved.
+// -------------------------------------------------------------------------
+
+using QProtocol.DataStreaming.Headers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace QProtocol.DataStreaming.DataPackets
+{
+    public class GpsDataPacket
+    {
+        private readonly uint binarySize = 0;
+
+        /// <summary>
+        /// A reference the Generic Channel Header received in the Payload.
+        /// </summary>
+        public GenericChannelHeader GenericChannelHeader { get; }
+
+        /// <summary>
+        /// A reference to the Specific Channel Header received in the Payload. It contains additional statical information.
+        /// </summary>
+        public GpsChannelHeader GpsChannelHeader { get; }
+
+        /// <summary>
+        /// A list of messages received in the Payload.
+        /// </summary>
+        public byte[] MessageList { get; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="GpsDataPacket"/> class and copies the data from the payload into the properties.
+        /// </summary>
+        /// <param name="genericChannelHeader">An instance of the generic channel header.</param>
+        /// <param name="gpsChannelHeader">An instance of the specific channel header.</param>
+        /// <param name="reader">The binary reader used to copy the data from.</param>
+        public GpsDataPacket(GenericChannelHeader genericChannelHeader, GpsChannelHeader gpsChannelHeader, BinaryReader reader)
+        {
+            GenericChannelHeader = genericChannelHeader;
+            GpsChannelHeader = gpsChannelHeader;
+            MessageList = reader.ReadBytes((int)genericChannelHeader.ChannelDataSize);
+            binarySize = genericChannelHeader.ChannelDataSize;
+        }
+
+        /// <summary>
+        /// Returns the number of bytes copied from the stream.
+        /// </summary>
+        /// <returns>Number of Bytes copied.</returns>
+        public uint GetBinarySize()
+        {
+            return binarySize;
+        }
+    }
+}
